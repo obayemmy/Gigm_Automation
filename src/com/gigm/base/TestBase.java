48a88129b7fda4b2ac48750f5541050d864b232b
obayemmy <obayemmy92@gmail.com>
2020-02-06 21:55:08 +0100

    updated the test script

--- src/com/gigm/base/TestBase.java
+++ src/com/gigm/base/TestBase.java
@@ -8,16 +8,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
 
 import com.gigm.utility.Utility;
-import com.sun.tools.sjavac.Log;
 
 public class TestBase {
 	public WebDriver driver ;
